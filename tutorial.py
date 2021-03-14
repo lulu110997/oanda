8a48257f64a8081fe91ee712cdeb1cb698f5b6a8
lulu110997 <53894562+lulu110997@users.noreply.github.com>
2021-03-15 02:03:50 +1100

    Update tutorial.py

--- tutorial.py
+++ tutorial.py
@@ -1,22 +1,10 @@
-'''
-import json
-from oandapyV20 import API    # the client
-import oandapyV20.endpoints.trades as trades
-
-client = API(access_token=access_token)
-
-# request trades list
-r = trades.TradesList(accountID)
-rv = client.request(r)
-print("RESPONSE:{escape_char}n{}".format(json.dumps(rv, indent=2)))
- '''
 import configparser
-# import pandas as pd
-# import json
-# import oandapyV20.endpoints.accounts as accounts
-import datetime
+import pandas as pd
+from datetime import datetime
 import oandapyV20 as opy
 import oandapyV20.endpoints.instruments as instruments
+import mplfinance as mpf
+import dateutil.parser
 
 # Read the ini file to obtain the account id and access token
 config = configparser.ConfigParser()
@@ -27,12 +15,6 @@ environment = config['oanda']['environment']
 
 oanda = opy.API(access_token, environment)
 
-## Instruments that this account is allowed to trade ##
-# req = accounts.AccountInstruments(account_id)
-# rcv = oanda.request(req)
-# print(json.dumps(rcv, indent=2))
-## End ##
-
 params = {
   "count": 5,
   "granularity": "H6"
@@ -41,6 +23,24 @@ params = {
 r = instruments.InstrumentsCandles(instrument="USD_CAD",
                                     params=params)
 oanda.request(r)
-print(r.response['candles'][1]['time'])
-timestamp = datetime.datetime.fromtimestamp(1500000000)
-print(timestamp.strftime('%Y-%m-%d %H:%M:%S'))
+candles = r.response['candles']
+for i in candles:
+    i.pop('complete')
+    i['time'] = dateutil.parser.isoparse(i['time'])
+    i['time'] = datetime.timestamp(i['time'])
+    olhc = i.pop('mid')
+    i['Date'] = i.pop('time')
+    i['Volume'] = i.pop('volume')
+    i['Open'] = float(olhc['o'])
+    i['High'] = float(olhc['h'])
+    i['Low'] = float(olhc['l'])
+    i['Close'] = float(olhc['c'])
+    i['Volume'] = float(i['Volume'])
+
+print(candles)
+df = pd.DataFrame(candles)
+df['Datetime'] = pd.to_datetime(df['Date'])
+df = df.set_index('Datetime')
+df = df.drop(['Date'], axis=1)
+df.to_csv('C:\\Users\\louis\\Downloads\\del\\csv.csv')
+mpf.plot(df,type='candle', mav=(3), volume=True)
