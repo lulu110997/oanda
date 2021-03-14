76be6837d8744da31d564b3fcc7730e9d5b99fdf
lulu110997 <53894562+lulu110997@users.noreply.github.com>
2021-03-14 20:57:03 +1100

    Update tutorial.py

--- tutorial.py
+++ tutorial.py
@@ -9,13 +9,13 @@ client = API(access_token=access_token)
 r = trades.TradesList(accountID)
 rv = client.request(r)
 print("RESPONSE:{escape_char}n{}".format(json.dumps(rv, indent=2)))
-'''
+ '''
 import configparser
 import pandas as pd
 import oandapyV20 as opy
-import oandapyV20.endpoints.accounts as accounts
 import json
-
+import oandapyV20.endpoints.accounts as accounts
+import oandapyV20.endpoints.instruments as instruments
 
 # Read the ini file to obtain the account id and access token
 config = configparser.ConfigParser()
@@ -25,7 +25,19 @@ access_token = config['oanda']['access_token']
 environment = config['oanda']['environment']
 
 oanda = opy.API(access_token, environment)
-r = accounts.AccountInstruments(account_id)
-rv = oanda.request(r)
-print(json.dumps(rv, indent=2))
 
+## Instruments that this account is allowed to trade ##
+# req = accounts.AccountInstruments(account_id)
+# rcv = oanda.request(req)
+# print(json.dumps(rcv, indent=2))
+## End ##
+
+params = {
+  "count": 5,
+  "granularity": "H6"
+}
+
+r = instruments.InstrumentsCandles(instrument="USD_CAD",
+                                    params=params)
+oanda.request(r)
+print(r.response['candles'][1]['mid'])
